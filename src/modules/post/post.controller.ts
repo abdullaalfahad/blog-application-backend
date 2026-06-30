f510a3d6a98edb9ded19eafb2fb2c52dcaed0469
abdullaalfahad <aafahad02@gmail.com>
2026-06-30 20:25:05 +0600

    feat: add post route create

---
+++ src/modules/post/post.controller.ts
@@ -0,0 +1,15 @@
+import type { Request, Response } from 'express';
+import { postService } from './post.service';
+
+const createPost = async (req: Request, res: Response) => {
+  try {
+    const result = await postService.createPost(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ message: 'Post creation failed', details: error });
+  }
+};
+
+export const postController = {
+  createPost,
+};
