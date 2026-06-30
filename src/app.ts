096eac9fef58ad8da57318d47009f2f5730b9bb2
abdullaalfahad <aafahad02@gmail.com>
2026-06-30 23:56:32 +0600

    feat: create user & sign up using better auth

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
+import { toNodeHandler } from 'better-auth/node';
+import cors from 'cors';
 import express, { type Application } from 'express';
+import { auth } from './lib/auth';
 import { postRouter } from './modules/post/post.router';
 
 const app: Application = express();
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.APP_URL,
+    credentials: true,
+  })
+);
 
+app.use('/api/auth/*splat', toNodeHandler(auth));
+
+app.use(express.json());
 app.use('/api/posts', postRouter);
 
 app.get('/', (req, res) => {
---
+++ src/lib/auth.ts
@@ -0,0 +1,31 @@
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { prisma } from './prisma';
+
+export const auth = betterAuth({
+  trustedOrigins: [process.env.APP_URL!],
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  user: {
+    additionalFields: {
+      role: {
+        type: 'string',
+        defaultValue: 'USER',
+        required: false,
+      },
+      phone: {
+        type: 'string',
+        required: false,
+      },
+      status: {
+        type: 'string',
+        defaultValue: 'ACTIVE',
+        required: false,
+      },
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+});
